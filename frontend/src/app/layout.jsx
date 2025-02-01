f2f84cdc15a23503a5b86593d2c9ff809e23a577
SaTriX <166950004+SaTriX43@users.noreply.github.com>
2025-02-01 16:11:58 -0500

    agregarmos metadatos a la pagina

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -3,6 +3,7 @@ import "../estilos/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Importa los estilos manualmente
 import { Suspense } from "react";
+import Head from "next/head";
 config.autoAddCss = false; // Deshabilita la carga automática de CSS
 
 
@@ -10,6 +11,24 @@ config.autoAddCss = false; // Deshabilita la carga automática de CSS
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Calistenia en Ecuador - Encuentra parques de calistenia</title>
+        <meta name="description" content="Descubre los mejores parques de calistenia en Ecuador. Información sobre ubicaciones, reseñas y más." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="keywords" content="calistenia, Ecuador, parques, entrenamiento, fitness" />
+        {/* opne grahp  */}
+        <meta property="og:title" content="Calistenia Ecuador" />
+        <meta property="og:description" content="Descubre los mejores parques de calistenia en Ecuador." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://app-calistenia.vercel.app/" />
+        <meta property="og:image" content="https://res.cloudinary.com/do2t2cxvo/image/upload/v1738339923/parques/cpk1ypyi8mj1kuxxm51w.jpg" />
+        {/* Twitter Cards  */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Calistenia Ecuador" />
+        <meta name="twitter:description" content="Descubre los mejores parques de calistenia en Ecuador." />
+        <meta name="twitter:image" content="https://res.cloudinary.com/do2t2cxvo/image/upload/v1738339923/parques/cpk1ypyi8mj1kuxxm51w.jpg" />
+      </Head>
       <body>
         <Suspense>
           <BarraNavegacion/>
--- frontend/src/app/parques/[id]/page.jsx
+++ frontend/src/app/parques/[id]/page.jsx
@@ -7,6 +7,7 @@ import Styles from './page.module.css'
 import Detalles from "./componentesDetalle/Detalles";
 import Mapa from "../../../components/parques/Mapa/Mapa";
 import DetallePublicidad from "./componentesDetalle/DetallePublicidad";
+import Head from "next/head";
 
 export default function DetalleParque() {
   const [parques, setParques] = useState([]) 
@@ -37,27 +38,42 @@ export default function DetalleParque() {
   
 
   return (
-    <section className={Styles['parques__detalle-parque']}>
-      <Detalles
-        imagen={parque.imagenes?.[0]}
-        nombre={parque.nombre}
-        direccion={parque.direccion}
-        puntuacion={parque.puntuacion}
-        canton={parque.canton}
-        pais= {parque.pais}
-        continente={parque.continente}
-      />
-      <div className={Styles['parques__detalle-parque-divisor']}>
-        <Mapa 
-          latitud={parque.latitud} 
-          longitud={parque.longitud} 
-          ubicaciones={parques}
-          clases={'parques__detalle-parque-mapa'}
+    <>
+      <Head>
+        <meta name="description" content={`Conoce ${parque.nombre}, ubicado en ${parque.direccion}. Descubre su puntuación, ubicación y otros detalles para que tu experiencia en calistenia sea inmejorable.`} />
+        <meta property="og:title" content={`${parque.nombre} - Detalles del Parque`} />
+        <meta property="og:description" content={`Descubre ${parque.nombre} en ${parque.direccion}. Más detalles sobre este parque de calistenia en Ecuador.`} />
+        <meta property="og:image" content={parque.imagenes?.[0] || 'https://res.cloudinary.com/do2t2cxvo/image/upload/v1738339923/parques/cpk1ypyi8mj1kuxxm51w.jpg'} />
+        <meta property="og:url" content={`https://app-calistenia.vercel.app/parque/${id}`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`${parque.nombre} - Detalles del Parque`} />
+        <meta name="twitter:description" content={`Conoce ${parque.nombre} ubicado en ${parque.direccion}.`} />
+        <meta name="twitter:image" content={parque.imagenes?.[0] || 'https://res.cloudinary.com/do2t2cxvo/image/upload/v1738339923/parques/cpk1ypyi8mj1kuxxm51w.jpg'} />
+      </Head>
+
+      <section className={Styles['parques__detalle-parque']}>
+        <Detalles
+          imagen={parque.imagenes?.[0]}
+          nombre={parque.nombre}
+          direccion={parque.direccion}
+          puntuacion={parque.puntuacion}
+          canton={parque.canton}
+          pais= {parque.pais}
+          continente={parque.continente}
         />
-        <DetallePublicidad 
-          parques= {parques}
-        />
-      </div>
-    </section>
+        <div className={Styles['parques__detalle-parque-divisor']}>
+          <Mapa 
+            latitud={parque.latitud} 
+            longitud={parque.longitud} 
+            ubicaciones={parques}
+            clases={'parques__detalle-parque-mapa'}
+          />
+          <DetallePublicidad 
+            parques= {parques}
+          />
+        </div>
+      </section>
+    </>
+    
   )
 }
